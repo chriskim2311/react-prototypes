9f41aad649b752b4c158cacf83d8ac167e13964a
chriskim2311 <chriskim2311@gmail.com>
2018-11-11 09:49:52 -0800

    finished functionality

---
+++ src/components/table.js
@@ -0,0 +1,46 @@
+// import React from 'react';
+
+
+// export default (props) => {
+
+//     const tableRows = props.data.map((item,index) => {
+//         return (
+//             <tr key = {index}>
+//             <td>{item.name}</td>
+//             <td>{item.course}</td>
+//             <td>{item.grade}</td>    
+//             </tr>
+//         )
+//     });
+//     return (
+//         <table className="table">
+//             <thead className="thead-inverse">
+//                 <tr>
+//                     <th>Name</th>
+//                     <th>Course</th>
+//                     <th>Grade</th>
+//                 </tr>
+//             </thead>
+//             <tbody>
+//                 {/* <tr>
+//                     <td>James Harden</td>
+//                     <td>Math</td>
+//                     <td>90</td>
+//                 </tr>
+//                 <tr>
+//                     <td>Kobe Bryant</td>
+//                     <td>Physical Education</td>
+//                     <td>98</td>
+//                 </tr>
+//                 <tr>
+//                     <td>Tracy McGrady</td>
+//                     <td>Biology</td>
+//                     <td>79</td>
+//                 </tr> */}
+//                 {tableRows}    
+
+
+//             </tbody>
+//         </table>
+//     )
+// }
