e04e6eb1de37a48757f21269513efb58dc5ab7ca
chriskim2311 <chriskim2311@gmail.com>
2018-11-13 22:39:12 -0800

    finished contact form

---
+++ src/data/contacts.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        firstName: 'Chris',
+        lastName: 'Kim',
+        phone: '123-145-1123',
+        email: 'superman@google.com'
+    },
+    {
+        firstName: 'James',
+        lastName: 'Harden',
+        phone: '785-328-9020',
+        email: 'rockets@google.com'
+    },
+    {
+        firstName: 'Bruce',
+        lastName: 'Wayne',
+        phone: '201-725-5692',
+        email: 'theRealBatman@google.com'
+    }
+ ];
